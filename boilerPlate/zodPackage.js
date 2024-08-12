67bae70cc8a6f3addadd74c05db358dee303650b
Yaseen KC <ymkcmpd@gmail.com>
2024-08-12 18:37:45 +0530

    modify zodPackage

--- boilerPlate/zodPackage.js
+++ boilerPlate/zodPackage.js
@@ -1,35 +1,19 @@
-const express = require("express");
-const app = express();
 const zod = require("zod");
-// const schema = zod.array(zod.number());
-const schema = zod.object({
-  email: zod.string(),
-  password: zod.string(),
-  country: zod.literal("IN").or(zod.literal("US")),
-  kidney: zod.array(zod.number()),
-});
 
-app.use(express.json());
-
-app.post("/totalKidney", (req, res) => {
-  const kidney = req.body.kidney;
-  const response = schema.safeParse(kidney);
-  if (!response.success) {
-    res.status(411).json({ msg: "Invalid Input" });
-  } else {
-    res.send({
-      response,
-    });
-  }
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({
-    msg: "Something is wrong",
+function validateData(n) {
+  const schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+    country: zod.literal("IN").or(zod.literal("US")),
+    kidney: zod.array(zod.number()),
   });
-});
+  const response = schema.safeParse(n);
+  console.log(response);
+}
 
-app.listen(3000, () => {
-  console.log("Server Started");
+validateData({
+  email: "ymkcmpd@gmail.com",
+  password: "1,2,3,4,5,6",
+  country: "IN",
+  kidney: [1],
 });
