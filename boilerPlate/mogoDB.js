329e14828275c504062f2ca5bae2648b4cc0f2d9
Yaseen KC <ymkcmpd@gmail.com>
2024-08-17 03:10:18 +0530

    efficient way

--- boilerPlate/mogoDB.js
+++ boilerPlate/mogoDB.js
@@ -1,25 +1,44 @@
 // Mongoose HandsON
+const express = require("express");
+const app = express;
 const mongoose = require("mongoose");
 
-const uri =
+const url =
   "mongodb+srv://yaseen:nQBNxuXShSjfG6AI@cluster0.hhiek.mongodb.net/myDataBase?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
-  .connect(uri)
+  .connect(url)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("Connection error", err));
 
 const User = mongoose.model("Users", {
-  name: String,
   username: String,
   email: String,
-}); 
-
-const user = new User({
-  name: "Zildjian",
-  email: "spam@gmail.com",
-  username: "@Zildjian",
+  password: String,
 });
-user
-  .save()
-  .then(() => console.log("Done"))
-  .catch((err) => console.error("Save error", err));
+
+app.post("/signup", async (req, res) => {
+  //   const username = req.body.username;
+  //   const email = req.body.email;
+  //   const password = req.body.password;
+  const { username, password, email } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+    const user = new User({
+      username,
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (err) {
+    console.error("Save error", err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
