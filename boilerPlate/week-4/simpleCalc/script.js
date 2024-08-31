c517d19cadf643c3699cbc0e54aaf9db63aef8bb
Yaseen KC <ymkcmpd@gmail.com>
2024-08-31 10:33:07 +0530

    js

--- boilerPlate/week-4/simpleCalc/script.js
+++ boilerPlate/week-4/simpleCalc/script.js
@@ -1,6 +1,17 @@
-function populateDiv() {
-  const element = document.getElementById("finalSum");
+async function populateDiv() {
   const a = document.getElementById("firstNumber").value;
   const b = document.getElementById("secondNumber").value;
-  element.innerHTML = parseInt(a) + parseInt(b);
+
+  try {
+    const response = await fetch(
+      "http://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b
+    );
+    if (!response.ok) {
+      throw new Error("Error");
+    }
+    const ans = await response.text();
+    document.getElementById("finalSum").innerHTML = ans;
+  } catch (error) {
+    console.error(error);
+  }
 }
